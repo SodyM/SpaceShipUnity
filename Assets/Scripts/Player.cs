453bf8c54e35069f4e4a591af4a3de6aa5258e95
Michal Sodomka <m.sodomka@equitania.com>
2016-03-24 00:38:59 +0100

    Prvni verze nepratel

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	/// <summary>
 	/// 1 - The speed of the ship
-	/// </summary>
 	public Vector2 speed = new Vector2(50, 50);
 
 	// 2 - Store the movement
@@ -17,24 +15,31 @@ public class Player : MonoBehaviour {
 		float inputX = Input.GetAxis("Horizontal");
 		float inputY = Input.GetAxis("Vertical");
 
+		bool spaceUp = Input.GetKeyDown(KeyCode.Space);
+		if (spaceUp)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				weapon.Attack (false);	
+			}
+		}
+
 		// 4 - Movement per direction
-		movement = new Vector2(
-			speed.x * inputX,
-			speed.y * inputY);
+		movement = new Vector2(speed.x * inputX, speed.y * inputY);
 	}
 
 	void FixedUpdate()
 	{
+		// move player
 		GetComponent<Rigidbody2D>().velocity = movement;
 	}
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		
 	}
 
 	void OnDestroy()
 	{
-		
 	}
 }
---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Projectile behavior
+/// </summary>
+public class ShotScript : MonoBehaviour
+{
+	// 1 - Designer variables
+
+	/// <summary>
+	/// Damage inflicted
+	/// </summary>
+	public int damage = 1;
+
+	/// <summary>
+	/// Projectile damage player or enemies?
+	/// </summary>
+	public bool isEnemyShot = false;
+
+	public float destoryAfterInterval = 5;
+
+	void Start()
+	{
+		// 2 - Limited time to live to avoid any leak
+		Destroy(gameObject, destoryAfterInterval); // 20sec
+	}
+}
